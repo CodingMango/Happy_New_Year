66895e39a00285dd444867d63a33570247985e04
CodingMango <Jabbarkhan562005@gmail.com>
2024-12-31 16:43:53 +0530

    Add files via upload
    Arduino Sketch for basic operation of '16*2 I2C LCD',  'Seven Segmented Display'.

---
+++ sketch.ino
@@ -0,0 +1,155 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+const int A=2,B=3,C=4,D=5,E=6,F=8,G=7;
+LiquidCrystal_I2C lcd(0x27,  16, 2);
+void setup() {
+  lcd.init();
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  //Ensuring All LED's are in OFF state
+ 
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  //starting Pattern
+  digitalWrite(A,LOW);
+  delay(300);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  delay(300);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  delay(300);
+  digitalWrite(C,HIGH);
+  digitalWrite(D,LOW);
+  delay(300);
+  digitalWrite(D,HIGH);
+  digitalWrite(E,LOW);
+  delay(300);
+  digitalWrite(E,HIGH);
+  digitalWrite(F,LOW);
+  delay(300);
+  digitalWrite(F,HIGH);
+  digitalWrite(A,LOW);
+  delay(300);
+  digitalWrite(A,HIGH);
+  digitalWrite(G,LOW);
+  delay(300);
+  digitalWrite(G,HIGH);
+  //COUNTDOWN
+  delay(1000);
+  //5
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  delay(1200);
+//4
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  delay(1200);
+  //3
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  delay(1200);
+  //2
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+  
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  delay(1200);
+  //1
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  delay(1200);
+  //0
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  lcd.backlight();
+}
+void loop() {
+  delay(1000);
+  lcd.setCursor(1, 0);
+  lcd.print("HAPPY NEW YEAR");
+  lcd.setCursor(0, 1);
+  delay(100);
+  lcd.print("******2024******");
+  lcd.setCursor(0, 1);
+  delay(1500);
+  lcd.print("~~~~~~2025~~~~~~");
+}
